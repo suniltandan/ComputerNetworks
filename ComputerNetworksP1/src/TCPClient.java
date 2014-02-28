a96ccab56c13f7c5a1844275fb2b03b1bc72e004
suniltandan <sunil_tandan@hotmail.com>
2014-02-28 12:40:56 +0100

    client aangepast

--- ComputerNetworksP1/src/TCPClient.java
+++ ComputerNetworksP1/src/TCPClient.java
@@ -3,18 +3,94 @@ import java.net.*;
 
 class TCPClient {
 	public static void main(String argv[]) throws Exception {
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
-				System.in));
-		Socket clientSocket = new Socket("localhost", 6789);
-		System.out.println("Connection Established");
-		DataOutputStream outToServer = new DataOutputStream(
-				clientSocket.getOutputStream());
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
-				clientSocket.getInputStream()));
-		String sentence = inFromUser.readLine();
-		outToServer.writeBytes(sentence + '\n');
-		String modifiedSentence = inFromServer.readLine();
-		System.out.println("FROM SERVER: " + modifiedSentence);
-		clientSocket.close();
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		boolean read=false;
+		String server=""; int port=0;
+		while(!read){
+			try{
+			System.out.println("Enter connection server and port: \nFormat: <server> <port>");
+			String[] serverPort = input.readLine().split(" ");
+			server = serverPort[0];
+			port = Integer.parseInt(serverPort[1]);
+			read=true;
+			}
+			catch(Exception exp){
+				System.out.println("Wrong Fromat!");
+			}
+		}
+		TCPClient client1 = new TCPClient(server,port);
+		client1.startClient();
+		
 	}
+	
+	
+	private DataOutputStream outToServer;
+	private BufferedReader inFromServer,inFromUser;
+	private Socket clientSocket;
+	public TCPClient(String server, Integer port) throws UnknownHostException, IOException{
+		this.inFromUser = new BufferedReader(new InputStreamReader(
+				System.in));
+		this.clientSocket = new Socket(server,port);
+		System.out.println("Connection Established");
+		this.outToServer = new DataOutputStream(
+				clientSocket.getOutputStream());
+		this.inFromServer = new BufferedReader(new InputStreamReader(
+				clientSocket.getInputStream()));
+	}
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public void startClient() throws IOException {
+		boolean closeConnection=false;
+		while(!closeConnection){
+			String sentence = inFromUser.readLine();
+			if(sentence.contains("HTTP/1.0")){
+				handleHTTP10(sentence);
+				handleResponse();
+				closeConnection=true;
+			}
+			else if(sentence.contains("HTTP/1.1")){
+				handleHTTP11(sentence);
+				handleResponse();
+				sentence = inFromUser.readLine();
+			}
+		}
+	}
+	/**
+	 * 
+	 * @param sentence
+	 * @throws IOException
+	 */
+	private void handleHTTP10(String sentence) throws IOException {
+		outToServer.writeBytes(sentence+'\n' +'\n');
+		outToServer.flush();
+	
+		
+	}
+	/**
+	 * 
+	 * @param sentence
+	 * @throws IOException
+	 */
+	private void handleHTTP11(String sentence) throws IOException {
+		outToServer.writeBytes(sentence+'\n' +'\n'+"Host: www.google.com" +'\n');
+		outToServer.flush();
+		
+		
+	}
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	private void handleResponse() throws IOException {
+		String response = "";
+		String fullResponse="";
+		while ((response=inFromServer.readLine())!=null){
+			fullResponse += "\n"+response;
+		}
+		System.out.println("FROM SERVER: " + fullResponse);
+		
+	}
+	
 }
