21ce1207a6f8af424fa790cb0a5c5c2f9803f1dc
suniltandan <sunil_tandan@hotmail.com>
2014-03-13 18:52:22 +0100

    handler

--- ComputerNetworksP1/src/Handler.java
+++ ComputerNetworksP1/src/Handler.java
@@ -1,6 +1,8 @@
 import java.io.*;
 
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,19 +16,23 @@ class Handler implements Runnable {
 	Socket connectedClient = null;
 	BufferedReader inFromClient = null;
 	DataOutputStream outToClient = null;
-	
+
 	/**
 	 * Constructor for this handler. Gets the client and saves it.
+	 * 
 	 * @param client
 	 */
 	public Handler(Socket client) {
 		connectedClient = client;
 	}
+
 	private String Protocol;
 	private boolean closeConnection = false;
+
 	/**
-	 * If the request string has bad format then sends a bad request and closes connection.
-	 * Else initiates respond methods to process the other informations.
+	 * If the request string has bad format then sends a bad request and closes
+	 * connection. Else initiates respond methods to process the other
+	 * informations.
 	 */
 	public void run() {
 		try {
@@ -40,7 +46,7 @@ class Handler implements Runnable {
 			while (!closeConnection) {
 				String requestString = inFromClient.readLine();
 				System.out.println(requestString);
-				if(requestString==null)
+				if (requestString == null)
 					break;
 				String[] s = requestString.split(" ");
 				if (s.length != 3) {
@@ -49,17 +55,19 @@ class Handler implements Runnable {
 				} else {
 					String httpMethod = s[0];
 					String httpQueryString = s[1];
+					httpQueryString = URLDecoder.decode(httpQueryString, "UTF-8");
 					Protocol = s[2];
-					if(Protocol.equals("HTTP/1.0")||Protocol.equals("HTTP/1.1")){
+					if (Protocol.equals("HTTP/1.0")
+							|| Protocol.equals("HTTP/1.1")) {
 						respond(httpMethod, httpQueryString);
 					}
-//					if (Protocol.equals("HTTP/1.0"))
-//						respondHTTP10(httpMethod, httpQueryString);
-//					else if (Protocol.equals("HTTP/1.1"))
-//						respondHTTP11(httpMethod, httpQueryString);
+					// if (Protocol.equals("HTTP/1.0"))
+					// respondHTTP10(httpMethod, httpQueryString);
+					// else if (Protocol.equals("HTTP/1.1"))
+					// respondHTTP11(httpMethod, httpQueryString);
 					else {
-						sendResponse(400,"BAD REQUEST",false);
-						closeConnection=true;
+						sendResponse(400, "BAD REQUEST", false);
+						closeConnection = true;
 					}
 				}
 			}
@@ -67,17 +75,19 @@ class Handler implements Runnable {
 			inFromClient.close();
 			connectedClient.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+		System.out.println("Connection Closed");	
 		}
 	}
-		
+
 	/**
-	 * This is the method that shall respond to protocol HTTP/1.1.
-	 * It is possible to combine both protocols but to make it easier this has been implemented separately.
+	 * This is the method that shall respond to protocol HTTP/1.1. It is
+	 * possible to combine both protocols but to make it easier this has been
+	 * implemented separately.
+	 * 
 	 * @param httpMethod
-	 * 			GET/HEAD/POST/PUT
+	 *            GET/HEAD/POST/PUT
 	 * @param httpQueryString
-	 * 			URL
+	 *            URL
 	 * @throws Exception
 	 */
 	private void respond(String httpMethod, String httpQueryString)
@@ -89,50 +99,78 @@ class Handler implements Runnable {
 		boolean containsHost = false;
 		String connectionHeader = null;
 		// Read the complete HTTP Query
-		while (inFromClient.ready()) {
-			System.out.println(requestString);
-			requestString = inFromClient.readLine();
-			//Default connection type for http1.1 is to keep it alive.
-			if(requestString.toLowerCase().contains("connection")){
-				connectionHeader = requestString;
+		if (!httpMethod.equals("POST") && !httpMethod.equals("PUT")) {
+			while (inFromClient.ready()) {
+				System.out.println(requestString);
+				requestString = inFromClient.readLine();
+				// Default connection type for http1.1 is to keep it alive.
+				if (requestString.toLowerCase().contains("connection")) {
+					connectionHeader = requestString;
+				}
+				if (requestString.toLowerCase().contains("host"))
+					containsHost = true;
+				if (bodystarted)
+					body += requestString + '\n';
+				if (requestString.isEmpty())
+					bodystarted = true;
+
 			}
-			if(requestString.toLowerCase().contains("host"))
-				containsHost=true;
-			if (bodystarted)
-				body += requestString + '\n';
-			if (requestString.isEmpty())
-				bodystarted = true;
-			
 		}
-		System.out.println("Body:"+body);
-		//If the http1.1 request does not contain HOST header then send a bad request and close the connection.
-		if(Protocol.equals("HTTP/1.1")&&!containsHost){
+		else{
+			String contentLength ="";
+			while (inFromClient.ready() && !bodystarted) {
+				System.out.println(requestString);
+				requestString = inFromClient.readLine();
+				// Default connection type for http1.1 is to keep it alive.
+				if (requestString.toLowerCase().contains("connection")) {
+					connectionHeader = requestString;
+				}
+				if(requestString.toLowerCase().contains("content-length"))
+					contentLength = requestString;
+				if (requestString.toLowerCase().contains("host"))
+					containsHost = true;
+				if (requestString.isEmpty())
+					bodystarted = true;
+
+			}
+			String[] s= contentLength.split(": ");
+			int length = Integer.parseInt(s[1]);
+			char[] charArray = new char[length];
+			inFromClient.read(charArray);
+			body = new String(charArray);
+			body = URLDecoder.decode(body, "UTF-8");
+			}
+		System.out.println("Body:" + body);
+		// If the http1.1 request does not contain HOST header then send a bad
+		// request and close the connection.
+		if (Protocol.equals("HTTP/1.1") && !containsHost) {
 			sendResponse(400, "BAD REQUEST", false);
-			closeConnection=true;
+			closeConnection = true;
 			return;
 		}
 		/**
-		 *This looks if the connection has to be closed or not.
-		 *HTTP/1.1 Default case: keep-alive
-		 *HTTP/1.0 Default case: close
-		 *Unless given otherwise. If it is neither then sends a bad request and closes the connection.
+		 * This looks if the connection has to be closed or not. HTTP/1.1
+		 * Default case: keep-alive HTTP/1.0 Default case: close Unless given
+		 * otherwise. If it is neither then sends a bad request and closes the
+		 * connection.
 		 */
-		if(Protocol.equals("HTTP/1.1")&&connectionHeader!=null){
-			if(connectionHeader.toLowerCase().contains("close"))
-				closeConnection=true;
-			else if(!connectionHeader.toLowerCase().contains("keep-alive")){
+		if (Protocol.equals("HTTP/1.1") && connectionHeader != null) {
+			if (connectionHeader.toLowerCase().contains("close"))
+				closeConnection = true;
+			else if (!connectionHeader.toLowerCase().contains("keep-alive")) {
 				sendResponse(400, "BAD REQUEST", false);
-				closeConnection=true;
+				closeConnection = true;
 				return;
 			}
-		}
-		else if(Protocol.equals("HTTP/1.0")){
+		} else if (Protocol.equals("HTTP/1.0")) {
 			closeConnection = true;
-			if(connectionHeader!=null && connectionHeader.toLowerCase().contains("keep-alive"))
-				closeConnection=false;
-			else if(connectionHeader!=null && !connectionHeader.toLowerCase().contains("close")){
+			if (connectionHeader != null
+					&& connectionHeader.toLowerCase().contains("keep-alive"))
+				closeConnection = false;
+			else if (connectionHeader != null
+					&& !connectionHeader.toLowerCase().contains("close")) {
 				sendResponse(400, "BAD REQUEST", false);
-				closeConnection=true;
+				closeConnection = true;
 				return;
 			}
 		}
@@ -153,13 +191,14 @@ class Handler implements Runnable {
 				if (new File(fileName).isFile()) {
 					sendResponse(200, fileName, true);
 				} else {
-					sendResponse(
-							404,
-							"<b>The Requested resource not found ....</b>",false);
+					sendResponse(404,
+							"<b>The Requested resource not found ....</b>",
+							false);
 				}
 			}
 		} else if (httpMethod.equals("PUT")) {
-			File textFile = new File("PutFrom"+connectedClient.getPort()+".txt");
+			File textFile = new File("PutFrom" + connectedClient.getPort()
+					+ ".txt");
 			textFile.createNewFile();
 			FileOutputStream fout = new FileOutputStream(textFile);
 			fout.write(body.getBytes());
@@ -169,7 +208,8 @@ class Handler implements Runnable {
 			System.out.println("Response sent to put method");
 
 		} else if (httpMethod.equals("POST")) {
-			File textFile = new File("PostFrom"+connectedClient.getPort()+".txt");
+			File textFile = new File("PostFrom" + connectedClient.getPort()
+					+ ".txt");
 			textFile.createNewFile();
 			FileOutputStream fout = new FileOutputStream(textFile);
 			fout.write(body.getBytes());
@@ -178,27 +218,29 @@ class Handler implements Runnable {
 			sendResponse(200, textFile.getName(), true);
 			System.out.println("Response sent to post method");
 		}
-		//This is for the methods that have not yet been implemented.
-		else if(httpMethod.equals("")){
-			sendResponse(500,"Server Error, This kind of request is not implemented.",false);
+		// This is for the methods that have not yet been implemented.
+		else if (httpMethod.equals("")) {
+			sendResponse(500,
+					"Server Error, This kind of request is not implemented.",
+					false);
 		}
 
-		else{
-			sendResponse(400,"BAD REQUEST",false);
+		else {
+			sendResponse(400, "BAD REQUEST", false);
 			closeConnection = true;
 		}
 
 	}
 
-		/**
+	/**
 	 * Sends the response
 	 * 
 	 * @param statusCode
-	 * 			Code for the status 200 OK|400|404|500
+	 *            Code for the status 200 OK|400|404|500
 	 * @param responseString
-	 * 			
+	 * 
 	 * @param isFile
-	 * 			If the response is a file.
+	 *            If the response is a file.
 	 * @throws Exception
 	 */
 	private void sendResponse(int statusCode, String responseString,
@@ -212,13 +254,13 @@ class Handler implements Runnable {
 		FileInputStream fin = null;
 
 		if (statusCode == 200)
-			statusLine = Protocol+" 200 OK" + "\r\n";
+			statusLine = Protocol + " 200 OK" + "\r\n";
 		else if (statusCode == 404)
-			statusLine = Protocol+" 404 Not Found" + "\r\n";
+			statusLine = Protocol + " 404 Not Found" + "\r\n";
 		else if (statusCode == 400)
-			statusLine = Protocol+" 400 Bad Request" + "\r\n";
+			statusLine = Protocol + " 400 Bad Request" + "\r\n";
 		else if (statusCode == 500)
-			statusLine = Protocol+" 500 Internal Server Error" + "\r\n";
+			statusLine = Protocol + " 500 Internal Server Error" + "\r\n";
 		if (isFile) {
 			fileName = responseString;
 			fin = new FileInputStream(fileName);
@@ -226,7 +268,7 @@ class Handler implements Runnable {
 					+ Integer.toString(fin.available()) + "\r\n";
 			if (!fileName.endsWith(".htm") && !fileName.endsWith(".html"))
 				contentTypeLine = "Content-Type: \r\n";
-			
+
 		} else {
 			responseString = HTML_START + responseString + HTML_END;
 			contentLengthLine = "Content-Length: " + responseString.length()
@@ -240,12 +282,11 @@ class Handler implements Runnable {
 		outToClient.writeBytes("Connection: close\r\n");
 		outToClient.writeBytes("\r\n");
 
-		if (isFile){
+		if (isFile) {
 			sendFile(fin, outToClient);
-		
-		}
-		else
-			outToClient.writeBytes(responseString);
+
+		} else
+			outToClient.writeBytes(responseString +"\r\n");
 
 	}
 
