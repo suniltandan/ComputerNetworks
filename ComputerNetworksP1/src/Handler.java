409430adcbc52ae82d4480900e456d2185903c40
suniltandan <sunil_tandan@hotmail.com>
2014-03-13 13:48:36 +0100

    cleaned up a little

--- ComputerNetworksP1/src/Handler.java
+++ ComputerNetworksP1/src/Handler.java
@@ -1,11 +1,9 @@
 import java.io.*;
 
 import java.net.Socket;
-import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.StringTokenizer;
 
 class Handler implements Runnable {
 	static final String HTML_START = "<html>"
@@ -30,7 +28,6 @@ class Handler implements Runnable {
 	 * If the request string has bad format then sends a bad request and closes connection.
 	 * Else initiates respond methods to process the other informations.
 	 */
-	@SuppressWarnings("deprecation")
 	public void run() {
 		try {
 			System.out.println("The Client " + connectedClient.getInetAddress()
@@ -73,85 +70,7 @@ class Handler implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	/**
-	 * This is the method that shall respond to protocol HTTP/1.1.
-	 * It is possible to combine both protocols but to make it easier this has been implemented separately.
-	 * @param httpMethod
-	 * 			GET/HEAD/POST/PUT
-	 * @param httpQueryString
-	 * 			URL
-	 * @throws Exception
-	 */
-	private void respondHTTP11(String httpMethod, String httpQueryString)
-			throws Exception {
-		System.out.println("The HTTP request string is ....");
-		String requestString = "";
-		String body = "";
-		boolean bodystarted = false;
-		boolean containsHost = false;
-		// Read the complete HTTP Query
-		while (inFromClient.ready()) {
-			System.out.println(requestString);
-			requestString = inFromClient.readLine();
-			//Default connection type for http1.1 is to keep it alive.
-			if(requestString.toLowerCase().contains("connection")){
-				if(requestString.toLowerCase().contains("close"))
-					closeConnection=true;
-				else if(!requestString.toLowerCase().contains("keep-alive")){
-					sendResponse(400, "BAD REQUEST", false);
-					closeConnection=true;
-					return;
-				}
-			}
-			if(requestString.toLowerCase().contains("host"))
-				containsHost=true;
-			if (requestString.isEmpty())
-				bodystarted = true;
-			if (bodystarted)
-				body += requestString + '\n';
-		}
-		//If the request does not contain HOST header then send a bad request and close the connection.
-		if(!containsHost){
-			sendResponse(400, "BAD REQUEST", false);
-			closeConnection=true;
-			return;
-		}
-
-		if (httpMethod.equals("GET")) {
-			if (httpQueryString.equals("/")) {
-				byte[] bytes = Files.readAllBytes(Paths.get(htmlFolder
-						+ "index.html"));
-				String text = new String(bytes, StandardCharsets.UTF_8);
-				sendResponse(200, text, false);
-			} else {
-				// This is interpreted as a file name
-				String fileName = httpQueryString.replaceFirst("/", "");
-				fileName = (htmlFolder + fileName);
-				if (new File(fileName).isFile()) {
-					sendResponse(200, fileName, true);
-				} else {
-					sendResponse(
-							404,
-							"<b>The Requested resource not found ....</b>",false);
-				}
-			}
-		} else if (httpMethod.equals("PUT")) {
-
-		} else if (httpMethod.equals("POST")) {
-
-		}
-		//This is for the methods that have not yet been implemented.
-		else if(httpMethod.equals("")){
-			sendResponse(500,"Server Error, This kind of request is not implemented.",false);
-		}
-
-		else{
-			sendResponse(400,"BAD REQUEST",false);
-			closeConnection = true;
-		}
-
-	}
-	
+		
 	/**
 	 * This is the method that shall respond to protocol HTTP/1.1.
 	 * It is possible to combine both protocols but to make it easier this has been implemented separately.
@@ -271,61 +190,7 @@ class Handler implements Runnable {
 
 	}
 
-	/**
-	 * Method to handle the http 1.0 request
-	 * 
-	 * @param httpMethod
-	 *            Kind of method GET/PUT/POST
-	 * @param httpQueryString
-	 *            URL
-	 * @throws Exception
-	 */
-	private void respondHTTP10(String httpMethod, String httpQueryString)
-			throws Exception {
-		System.out.println("The HTTP request string is ....");
-		String requestString = "";
-		while (inFromClient.ready()) {
-			// Read the HTTP complete HTTP Query
-			System.out.println(requestString);
-			requestString = inFromClient.readLine();
-		}
-
-		if (httpMethod.equals("GET")) {
-			if (httpQueryString.equals("/")) {
-				byte[] bytes = Files.readAllBytes(Paths.get(htmlFolder
-						+ "index.html"));
-				String text = new String(bytes, StandardCharsets.UTF_8);
-				sendResponse(200, text, false);
-			} else {
-				// This is interpreted as a file name
-				String fileName = httpQueryString.replaceFirst("/", "");
-				fileName = (htmlFolder + fileName);
-				if (new File(fileName).isFile()) {
-					sendResponse(200, fileName, true);
-				} else {
-					sendResponse(
-							404,
-							"<b>The Requested resource not found ...."
-									+ "Usage: http://127.0.0.1:5000 or http://127.0.0.1:5000/</b>",
-							false);
-				}
-			}
-		} else if (httpMethod.equals("POST")) {
-
-		} else if (httpMethod.equals("POST")) {
-
-		}
-
-		else
-			sendResponse(
-					404,
-					"<b>The Requested resource not found ...."
-							+ "Usage: http://127.0.0.1:5000 or http://127.0.0.1:5000/</b>",
-					false);
-		this.closeConnection = true;
-	}
-
-	/**
+		/**
 	 * Sends the response
 	 * 
 	 * @param statusCode
@@ -377,7 +242,6 @@ class Handler implements Runnable {
 
 		if (isFile){
 			sendFile(fin, outToClient);
-			 byte imageData[] = new byte[Integer.parseInt(Integer.toString(fin.available()))];
 		
 		}
 		else
